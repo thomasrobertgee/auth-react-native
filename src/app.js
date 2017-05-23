dd42c6fe7a03b7e4d03f8edb56f59c0fca245fa5
Thomas Gee <thomasrobertgee@gmail.com>
2017-05-23 11:03:52 +1000

    fixed the display bug, read through course Q&A and the issue was an update that broke the code, fixed by wrapping the logout button in CardSection tags.

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import { View } from 'react-native'
 import firebase from 'firebase'
-import { Header, Button, Spinner } from './components/common'
+import { Header, Button, Spinner, CardSection } from './components/common'
 import LoginForm from './components/LoginForm'
 
 class App extends Component {
@@ -30,9 +30,11 @@ class App extends Component {
     switch (this.state.loggedIn) {
       case true:
         return (
-          <Button onPress={() => firebase.auth().signOut()}>
-            Log Out
-          </Button>
+          <CardSection>
+            <Button onPress={() => firebase.auth().signOut()}>
+              Log Out
+            </Button>
+          </CardSection>
         )
       case false:
         return <LoginForm />
